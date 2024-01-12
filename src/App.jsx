44be9a7a1fc9ca322913905d1f77a94a1cd12ee5
Jeff Kinley <jkinley@icloud.com>
2024-01-12 14:11:32 -0800

    css updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import HelloWorld from './components/HelloWorld/HelloWorld.jsx';
 
 export default function App() {
   return (
-    <div className="contain">
+    <div>
       
       <HelloWorld />
-
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis dolor fugit unde expedita velit cupiditate dicta, consequuntur ipsum dignissimos dolores et ut quam earum, accusantium quibusdam sequi perspiciatis, laborum sapiente!</p>
-
-   
+      
     </div>
   );
 }
